e566421498b58825214496a2000071f01355b16f
BernardoFontes <bfontes.1997@gmail.com>
2020-11-26 16:11:57 -0300

    adicionando exercicio4

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio4.js
@@ -0,0 +1,15 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let result = 0;
+for (let i =0; i<numbers.length;i++){
+    result += numbers[i];
+}
+let media;
+media = result / numbers.length;
+console.log(media);
+
+if (media>20) {
+console.log("maior que 20");
+}
+else{
+console.log("menor ou igual a 20");
+}
